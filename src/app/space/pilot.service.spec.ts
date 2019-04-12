face7baac1eb333691c9dc1d015c9c4889c7be0a
Krzysztof Krysiak <krysiak86@gmail.com>
2019-04-12 14:33:29 +0200

    Add Pilot Service Tests

--- src/app/space/pilot.service.spec.ts
+++ src/app/space/pilot.service.spec.ts
@@ -1,12 +1,68 @@
 import { TestBed } from '@angular/core/testing';
-
 import { PilotService } from './pilot.service';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
+import { Pilot, PilotAttrs } from './pilot';
 
-describe('PilotService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+@Injectable()
+export class HttpClientMock extends HttpClient {
+    constructor() {
+        super(null);
+    }
+}
 
-  it('should be created', () => {
-    const service: PilotService = TestBed.get(PilotService);
-    expect(service).toBeTruthy();
+fdescribe('PilotService', () => {
+  let pilotService: PilotService;
+  let http: HttpClient;
+
+  beforeEach(() => {
+    http = new HttpClientMock();
+    pilotService = new PilotService(http);
+  });
+
+  describe('getPilot', () => {
+    beforeEach(() => {
+      const pilotAttrs = {id: 1, firstName: 'Mike', lastName: 'Tomsky', imageUrl: ''};
+      spyOn(http, 'get').and.returnValue(of(pilotAttrs));
+
+      it('should make a request for pilot', () => {
+        pilotService.getPilot(1);
+        expect(http.get).toHaveBeenCalledWith('/api/pilots/1');
+      });
+
+      it('should return pilot object', () => {
+        pilotService.getPilot(1).subscribe((pilot) => {
+          expect(pilot instanceof  Pilot).toBeTruthy();
+        });
+      });
+    });
+  });
+
+  describe('savePilot', () => {
+    let pilotAttrs: PilotAttrs;
+    describe('when pilot has id', () => {
+      beforeEach(() => {
+        pilotAttrs = {id: 1, firstName: 'Mike', lastName: 'Tomsky', imageUrl: ''};
+        spyOn(http, 'put').and.returnValue(of(pilotAttrs));
+      });
+
+      it('should make put request', () => {
+        pilotService.savePilot(pilotAttrs);
+        expect(http.put).toHaveBeenCalledWith('/api/pilots/1', pilotAttrs);
+      });
+    });
+
+    describe('when pilot has not id', () => {
+      beforeEach(() => {
+        pilotAttrs = {id: null, firstName: 'Mike', lastName: 'Tomsky', imageUrl: ''};
+        spyOn(http, 'post').and.returnValue(of(pilotAttrs));
+      });
+
+      it('should make post request', () => {
+        pilotService.savePilot(pilotAttrs);
+        expect(http.post).toHaveBeenCalledWith('/api/pilots', pilotAttrs);
+      });
+    });
   });
 });
