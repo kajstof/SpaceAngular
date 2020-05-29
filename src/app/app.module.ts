9bd9b7bc967c50e261390b28550d7fbb148f2cd4
Krzysztof Krysiak <krysiak86@gmail.com>
2020-05-29 15:39:49 +0200

    Add Component tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,11 @@ import { A2sCommModule } from 'a2s-comm';
 import { SpaceModule } from './space/space.module';
 import { HttpClientModule } from '@angular/common/http';
 import { BlackHoleComponent } from './black-hole/black-hole.component';
-import { HttpClientMockComponent } from './http-client-mock/http-client-mock.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BlackHoleComponent,
-    HttpClientMockComponent
   ],
   imports: [
     BrowserModule,
