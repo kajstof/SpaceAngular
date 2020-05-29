ab7272653ae701701477d88d2b166e76ca94fd1a
Krzysztof Krysiak <krysiak86@gmail.com>
2020-05-29 14:33:19 +0200

    Add tests for Pilot

--- src/app/space/pilot-validators.spec.ts
+++ src/app/space/pilot-validators.spec.ts
@@ -1,7 +1,35 @@
 import { PilotValidators } from './pilot-validators';
+import { FormControl } from '@angular/forms';
+import { ajax } from 'rxjs/ajax';
+import { of } from 'rxjs';
 
-describe('PilotValidators', () => {
+fdescribe('PilotValidators', () => {
   it('should create an instance', () => {
     expect(new PilotValidators()).toBeTruthy();
+  });
+  describe('pilotForbidden', () => {
+    describe('when value is empty', () => {
+      it('should return observable with null', () => {
+        const control = new FormControl('');
+        PilotValidators.pilotForbidden(control)
+          .subscribe((result) => expect(result).toBeNull());
+      });
+    });
+    describe('when value is forbidden', () => {
+      it('should return observable with validation object', () => {
+        const control = new FormControl('Ciapciak');
+        spyOn(ajax, 'getJSON').and.returnValue(of([{name: 'Ciapciak'}]));
+        PilotValidators.pilotForbidden(control)
+          .subscribe((result) => expect(result).toEqual({pilotForbidden: true}));
+      });
+    });
+    describe('when value is not forbidden', () => {
+      it('should return observable with null', () => {
+        const control = new FormControl('Adama');
+        spyOn(ajax, 'getJSON').and.returnValue(of([]));
+        PilotValidators.pilotForbidden(control)
+          .subscribe((result) => expect(result).toBeNull());
+      });
+    });
   });
 });
--- src/app/space/pilot.spec.ts
+++ src/app/space/pilot.spec.ts
@@ -1,7 +1,29 @@
 import { Pilot } from './pilot';
+import { FormControl } from '@angular/forms';
+import { PilotValidators } from './pilot-validators';
 
-describe('Pilot', () => {
+fdescribe('Pilot', () => {
   it('should create an instance', () => {
     expect(new Pilot()).toBeTruthy();
+  });
+  describe('pilotName', () => {
+    describe('when value is empty', () => {
+      it('should return null', () => {
+        const control = new FormControl('');
+        expect(PilotValidators.pilotName(control)).toBeNull();
+      });
+    });
+    describe('when starts from uppercase letter', () => {
+      it('should return null', () => {
+        const control = new FormControl('Adam');
+        expect(PilotValidators.pilotName(control)).toBeNull();
+      });
+    });
+    describe('when starts from lowcase letter', () => {
+      it('should return validation object', () => {
+        const control = new FormControl('adam');
+        expect(PilotValidators.pilotName(control)).toEqual({pilotName: true});
+      });
+    });
   });
 });
